a3b688899d982a676551d5e02fd1bf6016a72386
Ihor Shostak <kozcflxh@email.com>
2021-02-07 11:19:00 +0200

    maven project temp

---
+++ mycoolapp/src/main/java/com/ishostak/App.java
@@ -0,0 +1,13 @@
+package com.ishostak;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
