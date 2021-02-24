b76ffa7d6609f7bd206a93e8750fd7ad027ac847
Ihor Shostak <kozcflxh@email.com>
2021-02-24 14:21:10 +0200

    simple rest project for test

---
+++ solution-code-spring-rest-demo-hello-world/src/main/index.jsp
@@ -0,0 +1,11 @@
+<html>
+
+<body>
+<h1> SPRING REST DEMO </h1>
+
+<hr>
+
+<a href = "test/hello" >Hello </a>
+
+</body>
+</html>
---
+++ solution-code-spring-rest-demo-hello-world/src/main/java/com/ishostak/springdemo/config/DemoAppConfig.java
@@ -0,0 +1,13 @@
+package com.ishostak.springdemo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.ishostak.springdemo")
+public class DemoAppConfig implements WebMvcConfigurer {
+
+}
