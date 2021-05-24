f8223d8ebef26756e49630c48bf037f3c4b9af24
Ihor Shostak <kozcflxh@email.com>
2021-05-24 15:04:31 +0300

    added spring devtools for autoreloading

---
+++ 02-devtools-demo/src/main/java/com/ishostak/springboot/demo/mycoolapp/rest/RestController.java
@@ -0,0 +1,21 @@
+package com.ishostak.springboot.demo.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDateTime;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+
+    //expose "/" endpoint Hello World
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello world " + LocalDateTime.now();
+    }
+
+    @GetMapping("/workout")
+    public String getWorkou() {
+        return "Run over the world";
+    }
+}
