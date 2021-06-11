e67c083c7a8ecc39e8e0fae8c57b1201bf8b1812
Ihor Shostak <kozcflxh@email.com>
2021-06-11 15:17:14 +0300

    changed default pass for endpoint and added some properties for pagination

--- 25_springdata/src/main/java/com/ishostak/springboot/cruddemo/dao/EmployeeRepository.java
+++ 25_springdata/src/main/java/com/ishostak/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,10 @@ package com.ishostak.springboot.cruddemo.dao;
 
 import com.ishostak.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//define new endpoint instead of default
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
